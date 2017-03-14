877ede90a4b5cde3c23cc89cf079237063884e98
Giuseppe Capizzi <g.capizzi@gmail.com>
2017-03-14 00:07:03 +0000

    [rust] First implementation of 'add' and 'list'

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,7 +1,37 @@
 pub struct TodoApp {}
 
+use std::fs::OpenOptions;
+use std::io::Read;
+use std::io::Write;
+
 impl TodoApp {
-    pub fn run(&self, args: Vec<String>) -> &str {
-        return "Hello, world!";
+    pub fn run(&self, args: Vec<String>) -> String {
+        return match args[0].as_ref() {
+            "add" => {
+                self.add(&args[1]);
+                "".to_string()
+            }
+            "list" => self.list(),
+            _ => "Unrecognized command".to_string(),
+        }
+    }
+
+    fn add(&self, text: &str) {
+        let mut file = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open("/Users/giuseppe/.todo")
+            .unwrap();
+        file.write_all((text.to_string() + "\n").as_bytes());
+    }
+
+    fn list(&self) -> String {
+        let mut file = OpenOptions::new()
+            .read(true)
+            .open("/Users/giuseppe/.todo")
+            .unwrap();
+        let mut buffer = String::new();
+        file.read_to_string(&mut buffer);
+        return buffer;
     }
 }
--- rust/tests/integration_test.rs
+++ rust/tests/integration_test.rs
@@ -4,9 +4,10 @@ extern crate todo_cli;
 fn it_adds_a_todo() {
     let writer_app = todo_cli::TodoApp {};
     writer_app.run(vec!["add".to_string(), "foo".to_string()]);
+    writer_app.run(vec!["add".to_string(), "bar".to_string()]);
 
     let reader_app = todo_cli::TodoApp {};
     let list_output = reader_app.run(vec!["list".to_string()]);
 
-    assert_eq!(list_output, "foo\n");
+    assert_eq!(list_output, "foo\nbar\n");
 }
